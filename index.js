e9ae1f133b88fcc23ad3d63960ae62028afcf5ec
imjeremiah <52246955+imjeremiah@users.noreply.github.com>
2024-05-23 12:59:31 -0700

    Add decimal functionality

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 function add(a, b) {
-    return Math.round(((a + b) * 100000000) / 100000000);
+    return a + b;
 }
 
 function subtract(a, b) {
-    return Math.round(((a - b) * 100000000) / 100000000);
+    return a - b;
 }
 
 function multiply(a, b) {
-    return Math.round(((a * b) * 100000000) / 100000000);
+    return a * b;
 }
 
 function divide(a, b) {
     if (a === 0 | b === 0) return alert("Not so fast!")
-        return Math.round(((a / b) * 100000000) / 100000000);
+        return a / b;
 }
 
 let operator;
@@ -39,16 +39,21 @@ buttons.forEach(button => {
             firstNumber = Number(display.textContent);
             operator = e.target.innerText;
             display.textContent = "0";
+            document.querySelector(".decimal").disabled = false;
         } 
         if (e.target.className.includes("equals")) {
             secondNumber = Number(display.textContent);
             display.textContent = operate(operator, firstNumber, secondNumber);
         }
         if (e.target.className.includes("clear")) {
+            document.querySelector(".decimal").disabled = false;
             display.textContent = "0";
             firstNumber = "";
             secondNumber = "";
             operator = "";
+        }
+        if (display.textContent.includes(".")) {
+            document.querySelector(".decimal").disabled = true;
         }
     });
 });

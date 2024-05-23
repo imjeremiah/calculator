c854da2b7b08a1c9edbbb5e86a1f4db8086b105e
imjeremiah <52246955+imjeremiah@users.noreply.github.com>
2024-05-23 12:40:45 -0700

    Add functionality for calculator to work

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 function add(a, b) {
-    return a + b;
+    return Math.round(((a + b) * 100000000) / 100000000);
 }
 
 function subtract(a, b) {
-    return a - b;
+    return Math.round(((a - b) * 100000000) / 100000000);
 }
 
 function multiply(a, b) {
-    return a * b;
+    return Math.round(((a * b) * 100000000) / 100000000);
 }
 
 function divide(a, b) {
-    return a / b;
+    if (a === 0 | b === 0) return alert("Not so fast!")
+        return Math.round(((a / b) * 100000000) / 100000000);
 }
 
 let operator;
@@ -24,3 +25,30 @@ function operate(operator, firstNumber, secondNumber) {
     if (operator === "*") return multiply(firstNumber, secondNumber);
     if (operator === "/") return divide(firstNumber, secondNumber);
 }
+
+let display = document.querySelector(".display");
+let buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        if (display.textContent === "0") display.textContent = "";
+        if (e.target.className.includes("number")) {
+            display.textContent += e.target.innerText;
+        } 
+        if (e.target.className.includes("operator")) {
+            firstNumber = Number(display.textContent);
+            operator = e.target.innerText;
+            display.textContent = "0";
+        } 
+        if (e.target.className.includes("equals")) {
+            secondNumber = Number(display.textContent);
+            display.textContent = operate(operator, firstNumber, secondNumber);
+        }
+        if (e.target.className.includes("clear")) {
+            display.textContent = "0";
+            firstNumber = "";
+            secondNumber = "";
+            operator = "";
+        }
+    });
+});
